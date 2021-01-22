1850233ace3058df1c945b13eb6a9c08a4657ca4
’val-dev-g’ <jajora78@gmail.com>
2021-01-22 15:53:45 +0100

    V1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte a = 5;
+		short b = 18000;
+		int c = 550000;
+		long d = 20000L;
+		float e = 2.8F;
+		double f = 3.6D;
+		char g = 'r';
+		boolean h = true;
+		String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+
+		System.out.println(a);
+		System.out.println(randomString);
+	}
+
+}
