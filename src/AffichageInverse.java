2294208199fb2fb99bcf013c1f73c52f978bab20
’val-dev-g’ <jajora78@gmail.com>
2021-01-25 15:28:26 +0100

    l'exo "Affiche inverse" terminé

--- src/AffichageInverse.java
+++ src/AffichageInverse.java
@@ -2,7 +2,7 @@ public class AffichageInverse {
 
  public static void main(String[] args) {
   int[] array ={1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
-  int[] destination = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int[] destination = new int[array.length];
   
    System.out.print("array ordre normal :");
 
@@ -16,11 +16,11 @@ public class AffichageInverse {
   for (int i = array.length -1; i >= 0; i--) {
    System.out.print(array[i]+" ");
   }
-  
-  for (int n : destination) {
-      System.out.print(n + " ");
+  System.out.println();
+  for (int i = 0; i < array.length; i++) {
+	  destination[i] = array[i];
+	  System.out.print(" " + destination[i]);
   }
-  System.out.print("copy Array");
-  System.arraycopy(array, 1, destination, 2, 6);
+
  }
 }
